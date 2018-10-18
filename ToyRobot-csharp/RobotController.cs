adaeec3592a1f36cf7f32586bf05fbbc83f240c9
aduty23 <aduty23@gmail.com>
2018-10-18 08:08:36 -0400

    Added a Robot controller for handling the input

---
+++ ToyRobot-csharp/RobotController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ToyRobot
+{
+    public class RobotController
+    {
+
+
+        public void addCommand(String command)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
