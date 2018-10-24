40cf644ca02054ab163f32310d2f43e46c7b0ceb
Aaron Duty <aaron@eksdyne.com>
2018-10-24 15:23:35 -0400

    Added implementation for the left command.

--- ToyRobot-csharp/Commands/LeftRobotCommand.cs
+++ ToyRobot-csharp/Commands/LeftRobotCommand.cs
@@ -1,6 +1,15 @@
 ﻿namespace ToyRobot.Commands
 {
-    internal class LeftRobotCommand : IRobotCommand
+    internal class LeftRobotCommand : AbstractRobotCommand, IRobotCommand
     {
+        public LeftRobotCommand(IRobot robot) : base(robot)
+        {
+        }
+
+        public void execute()
+        {
+            robot.Left();
+            outputWriter.WriteLine("Robot turned left.");
+        }
     }
 }
