f5b616877a272ca54b7055a4093ef51a60030ce0
Aaron Duty <aaron@eksdyne.com>
2018-10-24 15:33:21 -0400

    Implementation for Report command

--- ToyRobot-csharp/Commands/ReportRobotCommand.cs
+++ ToyRobot-csharp/Commands/ReportRobotCommand.cs
@@ -1,6 +1,14 @@
 ﻿namespace ToyRobot.Commands
 {
-    internal class ReportRobotCommand : IRobotCommand
+    internal class ReportRobotCommand : AbstractRobotCommand, IRobotCommand
     {
+        public ReportRobotCommand(IRobot robot) : base(robot)
+        {
+        }
+
+        public void execute()
+        {
+            outputWriter.WriteLine("Robot at " + robot.Report());
+        }
     }
 }
