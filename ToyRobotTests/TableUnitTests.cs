f3bedcf64e2918ae64b11416d8819a11fae80f62
Aaron Duty <aaron@eksdyne.com>
2018-10-26 15:04:46 -0400

    Tests to ensure negative or zero dimension length tables cannot be made
    Went ahead and implemented the exception and implementation too.

--- ToyRobotTests/TableUnitTests.cs
+++ ToyRobotTests/TableUnitTests.cs
@@ -6,5 +6,16 @@ namespace ToyRobotTests
 {
     public class TableUnitTests
     {
+        [Fact]
+        public void ThrowsOnNegativeDimensions()
+        {
+            Assert.Throws<NegativeOrZeroDimensionException>(() => new Table(-2, -3));
+        }
+
+        [Fact]
+        public void ThrowsOnZeroLengthDimension()
+        {
+            Assert.Throws<NegativeOrZeroDimensionException>(() => new Table(0, 0));
+        }
     }
 }
